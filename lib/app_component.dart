1b1ff00edd07ca29546fd84479a1e0180f02f4a2
kseregal <skablov@gmail.com>
2017-11-09 18:10:37 +0300

    Добавление разных вариантов использовагия директивы.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -15,7 +15,12 @@ import 'package:test_directive/src/jur/jur.dart';
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives, MaterialAutoSuggestInputComponent, DadataDirective, NgIf],
+  directives: const [
+    materialDirectives,
+    MaterialAutoSuggestInputComponent,
+    DadataDirective,
+    NgIf
+  ],
   providers: const [materialProviders],
 )
 class AppComponent {
@@ -25,19 +30,35 @@ class AppComponent {
   /// По умолчанию пол неизвестен. DADATA воспринимает параметр в таком виде, и выдает
   /// подсказки независимо от пола.
   String gender = 'UNKNOWN';
+
   /// Проверка наличия данных о поле в результатах выбранной подсказки и установка значения gender.
-  void setGender (SelectionModel model) {
-    if ( !model.isEmpty && model.selectedValues.first['data'] != null ) {
+  void setGender(SelectionModel model) {
+    if (!model.isEmpty && model.selectedValues.first['data'] != null) {
       gender = model.selectedValues.first['data']['gender'] ?? null;
     }
   }
 
+  /// Для региона:
+  /// Пустой список. Необходим для инициализации.
+  SelectionOptions regionOptions =
+      new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[])]);
+  SelectionModel regionSelectionModel =
+      new SelectionModel.withList(allowMulti: false);
+  ItemRenderer get itemRegionRenderer => (Map<String, dynamic> itemMap) {
+        return itemMap['data']['region_with_type'] ?? itemMap['data']['region'];
+      };
+  String regionInputText;
+
   /// Для города:
   /// Пустой список. Необходим для инициализации.
-  SelectionOptions cityOptions = new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[]) ]);
+  SelectionOptions cityOptions =
+      new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[])]);
   String cityInputText;
+
   /// Геттер класса реализации отрисовки элементов списка.
-  SelectionModel citySelectionModel = new SelectionModel.withList(allowMulti: false);
+  SelectionModel citySelectionModel =
+      new SelectionModel.withList(allowMulti: false);
+
   /// Обработка события выбора элемента списка подсказок.
 
   ItemRenderer get itemCityRenderer => _itemCityRenderer;
@@ -47,73 +68,95 @@ class AppComponent {
   String cityFiasId;
   String street;
   String streetTypeFull;
+
   /// Пустой список. Необходим для инициализации.
-  SelectionOptions options = new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[]) ]);
+  SelectionOptions options =
+      new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[])]);
   String inputText;
+
   /// Геттер класса реализации отрисовки элементов списка.
-  ComponentRenderer get myComponentRenderer =>  (_) => OneRowRendererComponent;
-  SelectionModel addressSelectionModel = new SelectionModel.withList(allowMulti: false);
+  ComponentRenderer get myComponentRenderer => (_) => OneRowRendererComponent;
+  SelectionModel addressSelectionModel =
+      new SelectionModel.withList(allowMulti: false);
+
   /// Обработка события выбора элемента списка подсказок.
   address_blur() {
-    cityFiasId = addressSelectionModel.selectedValues.first['data']['city_fias_id'];
-    cityKladrId = addressSelectionModel.selectedValues.first['data']['city_kladr_id'];
+    cityFiasId =
+        addressSelectionModel.selectedValues.first['data']['city_fias_id'];
+    cityKladrId =
+        addressSelectionModel.selectedValues.first['data']['city_kladr_id'];
     street = addressSelectionModel.selectedValues.first['data']['street'];
-    streetTypeFull = addressSelectionModel.selectedValues.first['data']['street_type_full'];
+    streetTypeFull =
+        addressSelectionModel.selectedValues.first['data']['street_type_full'];
   }
 
   /// Пустой список. Необходим для инициализации.
-  SelectionOptions fioOptions = new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[]) ]);
+  SelectionOptions fioOptions =
+      new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[])]);
   String fioInputText;
-  final SelectionModel surnameSelectionModel = new SelectionModel.withList(allowMulti: false);
+  final SelectionModel surnameSelectionModel =
+      new SelectionModel.withList(allowMulti: false);
 
   /// Контроллер выбора подсказки в поле Фамилия.
-  surname_blur() {    setGender(surnameSelectionModel);  }
+  surname_blur() {
+    setGender(surnameSelectionModel);
+  }
 
   /// Имя
   String nameInputText;
-  SelectionOptions nameOptions = new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[]) ]);
-  final SelectionModel nameSelectionModel = new SelectionModel.withList(allowMulti: false);
+  SelectionOptions nameOptions =
+      new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[])]);
+  final SelectionModel nameSelectionModel =
+      new SelectionModel.withList(allowMulti: false);
 
-  name_blur () {
+  name_blur() {
     setGender(nameSelectionModel);
   }
 
   /// БАНК.
   /// Пустой список. Необходим для инициализации.
-  SelectionOptions bankOptions = new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[]) ]);
+  SelectionOptions bankOptions =
+      new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[])]);
   String bankInputText;
+
   /// Геттер класса реализации отрисовки элементов списка.
-  ComponentRenderer get bankComponentRenderer =>  (_) => OneRowBankRendererComponent;
-  final SelectionModel bankSelectionModel = new SelectionModel.withList(allowMulti: false);
+  ComponentRenderer get bankComponentRenderer =>
+      (_) => OneRowBankRendererComponent;
+  final SelectionModel bankSelectionModel =
+      new SelectionModel.withList(allowMulti: false);
   Bank bank = null;
 
-  bank_blur () {
+  bank_blur() {
     Map selectedBank = bankSelectionModel.selectedValues.first;
     bank = new Bank.fromMap(selectedBank);
   }
 
   /// ОРГАНИЗАЦИЯ
   ///
-  SelectionOptions jurOptions = new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[]) ]);
+  SelectionOptions jurOptions =
+      new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[])]);
   String jurInputText;
+
   /// Геттер класса реализации отрисовки элементов списка.
-  ComponentRenderer get jurComponentRenderer =>  (_) => OneRowJurRendererComponent;
-  final SelectionModel jurSelectionModel = new SelectionModel.withList(allowMulti: false);
+  ComponentRenderer get jurComponentRenderer =>
+      (_) => OneRowJurRendererComponent;
+  final SelectionModel jurSelectionModel =
+      new SelectionModel.withList(allowMulti: false);
   Jur organization = null;
-  jur_blur () {
+  jur_blur() {
     Map selectedOrganization = jurSelectionModel.selectedValues.first;
     organization = new Jur.fromMap(selectedOrganization);
   }
 
-
   ///  EMAIL
-  SelectionOptions emailOptions = new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[]) ]);
+  SelectionOptions emailOptions =
+      new SelectionOptions([new OptionGroup<List<int>>(const <List<int>>[])]);
   String emailInputText;
-  final SelectionModel emailSelectionModel = new SelectionModel.withList(allowMulti: false);
+  final SelectionModel emailSelectionModel =
+      new SelectionModel.withList(allowMulti: false);
 }
 
-
-ItemRenderer _itemCityRenderer = ( Map<String, dynamic> itemMap) {
+ItemRenderer _itemCityRenderer = (Map<String, dynamic> itemMap) {
   return itemMap['data']['city_with_type'] ?? "";
 };
 
@@ -121,7 +164,7 @@ ItemRenderer _itemCityRenderer = ( Map<String, dynamic> itemMap) {
 /// Извлекает данные из хэша данные автоподсказки.
 /// Структуру объекта JSON ответа сервера можно посмотреть здесь
 /// https://dadata.ru/api/suggest/#response-address
-ItemRenderer _myRendener = ( Map<String, dynamic> itemMap) {
+ItemRenderer _myRendener = (Map<String, dynamic> itemMap) {
   return itemMap['value'] ?? "";
 };
 
@@ -129,10 +172,7 @@ ItemRenderer _myRendener = ( Map<String, dynamic> itemMap) {
 ///
 /// Реализует отрисовку одной строки выпадающего списка, извлекая нужные данные
 /// из объекта Map с порцией адресных данных.
-@Component(
-    selector: 'adres-renderer',
-    template: '''{{displayValue}}'''
-)
+@Component(selector: 'adres-renderer', template: '''{{displayValue}}''')
 class OneRowRendererComponent implements RendersValue {
   String displayValue = '';
 
@@ -142,7 +182,6 @@ class OneRowRendererComponent implements RendersValue {
     displayValue = _myRendener(addressMap);
   }
 }
-
 
 /// Компонент отображения одной строки списка подсказки банков.
 ///
@@ -154,15 +193,19 @@ class OneRowRendererComponent implements RendersValue {
     <div class="bank">
     {{bankFullName}}<br/> <span class="second_row">{{bankBic}} {{bankCity}}</span>
     </div>''',
-  styles: const ['.bank { border-bottom: 1px solid gray;} .second_row { font-size: 12px; color: gray;}']
-)
+    styles: const [
+      '.bank { border-bottom: 1px solid gray;} .second_row { font-size: 12px; color: gray;}'
+    ])
 class OneRowBankRendererComponent implements RendersValue {
   /// БИК.
   String bankBic = '';
+
   /// Наименование банка одной строкой (как показывается в списке подсказок).
   String bankFullName = '';
+
   /// SWIFT
   String bankSwift = '';
+
   /// Город
   String bankCity = '';
 
@@ -172,10 +215,11 @@ class OneRowBankRendererComponent implements RendersValue {
     bankFullName = addressMap['value'] ?? addressMap['unrestricted_value'];
     bankBic = addressMap['data']['bic'] ?? '';
     bankSwift = addressMap['data']['swift'] ?? '';
-    bankCity = ( addressMap['data']['address']['data'] != null ) ? addressMap['data']['address']['data']['city'] : '';
+    bankCity = (addressMap['data']['address']['data'] != null)
+        ? addressMap['data']['address']['data']['city']
+        : '';
   }
 }
-
 
 /// Компонент отображения одной строки списка подсказки банков.
 ///
@@ -187,13 +231,16 @@ class OneRowBankRendererComponent implements RendersValue {
     <div class="jur">
     {{fullName}}<br/> <span class="second_row">{{inn}} {{jurCity}}</span>
     </div>''',
-    styles: const ['.jur { border-bottom: 1px solid gray;} .second_row { font-size: 12px; color: gray;}']
-)
+    styles: const [
+      '.jur { border-bottom: 1px solid gray;} .second_row { font-size: 12px; color: gray;}'
+    ])
 class OneRowJurRendererComponent implements RendersValue {
   /// ИНН.
   String inn = '';
+
   /// Наименование организации одной строкой (как показывается в списке подсказок).
   String fullName = '';
+
   /// Город
   String jurCity = '';
 
@@ -202,6 +249,8 @@ class OneRowJurRendererComponent implements RendersValue {
   set value(jurMap) {
     fullName = jurMap['value'] ?? jurMap['unrestricted_value'];
     inn = jurMap['data']['inn'] ?? '';
-    jurCity = ( jurMap['data']['address']['data'] != null ) ? jurMap['data']['address']['data']['city'] : '';
+    jurCity = (jurMap['data']['address']['data'] != null)
+        ? jurMap['data']['address']['data']['city']
+        : '';
   }
 }
